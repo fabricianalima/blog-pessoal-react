4585b627f4bcbaebc16a33b57d88217a0c03452a
Fabriciana Lima <fabricianacorreialima@gmail.com>
2026-06-30 19:41:29 -0300

    Implementação da conexão com o backend

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,24 @@ import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Cadastro from './pages/cadastro/Cadastro'
 import Login from './pages/login/Login'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
 	return (
 		<>
-			<BrowserRouter>
-				<Navbar />
-				<div className="min-h-[80vh]">
-					<Routes>
-						<Route path="/" element={<Login />} />
-						<Route path="/home" element={<Home />} />
-						<Route path="/cadastro" element={<Cadastro />}
-						 />
-						
-					</Routes>
-				</div>
-				<Footer />
-			</BrowserRouter>
+			<AuthProvider>
+				<BrowserRouter>
+					<Navbar />
+					<div className="min-h-[80vh]">
+						<Routes>
+							<Route path="/" element={<Login />} />
+							<Route path="/home" element={<Home />} />
+							<Route path="/cadastro"	element={<Cadastro />}/>
+						</Routes>
+					</div>
+					<Footer />
+				</BrowserRouter>
+			</AuthProvider>
 		</>
 	)
 }
